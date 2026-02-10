a1506fe69188c736f0cb65d07fa0d2240063866e
waqar-ahmeddev <waqarkhano145@gmail.com>
2026-02-11 00:20:02 +0500

    Fix Search screen: corrected SliverAppBar and horizontal buttons layout

--- lib/search.dart
+++ lib/search.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+
 class Search extends StatelessWidget {
   const Search({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<String> Buttons = [
+      "Stay Here",
+      "Leave",
+      "How Are You",
+      "Keep it up",
+      "Nice",
+      "Will meet later",
+      "Not My fault",
+      "Your fault",
+      "ok",
+    ];
+
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            pinned: true,
             title: TextFormField(
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.search),
@@ -16,18 +30,36 @@ class Search extends StatelessWidget {
                 border: OutlineInputBorder(
                   borderSide: BorderSide.none,
                   borderRadius: BorderRadius.circular(10),
-                  ),
-               fillColor: Color.fromRGBO(220, 220, 220, 1),
-               filled: true,
-                )
+                ),
+                fillColor: Color.fromRGBO(220, 220, 220, 1),
+                filled: true,
               ),
-              actions: [IconButton(onPressed: (){
-
-              }, 
-              icon: Icon(Icons.person_add),
-              ),
-              ],
             ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.person_add),
+              ),
+            ],
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(50),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                    Buttons.length,
+                    (index) => Container(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        child: Text(Buttons[index]),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
